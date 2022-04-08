84dc7983336c7a1d80eb2bed1837ec039a1aa4c9
EntenKoeniq <81123713+EntenKoeniq@users.noreply.github.com>
2022-04-08 09:09:02 +0200

    Initial commit

---
+++ FirstScript/FirstScript/Main.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+using CoopClient;
+
+using GTA;
+
+namespace FirstScript
+{
+    [Serializable]
+    public class TestPacket
+    {
+        public string Content { get; set; }
+    }
+
+    public class Main : Script
+    {
+        public static readonly Dictionary<long, int> PlayerScore = new Dictionary<long, int>();
+        public Main()
+        {
+            COOPAPI.OnModPacketReceived += ModPacketReceived;
+            COOPAPI.OnChatMessage += ChatMessage;
+        }
+
+        private static void ModPacketReceived(long from, string mod, byte customID, byte[] bytes)
+        {
+            if (!mod.Equals("FirstScript"))
+            {
+                return;
+            }
+
+            bool playerFound = true;
+            var fromPlayer = COOPAPI.GetPlayer(from);
+            if (fromPlayer == null)
+            {
+                GTA.UI.Notification.Show($"Player by ID [{from}] not found!");
+                playerFound = false;
+            }
+
+            switch (customID)
+            {
+                case 0:
+                    GTA.UI.Notification.Show("customID ~b~0~s~ ~g~received~s~" + (playerFound ? $" from {fromPlayer.Username}!" : "!"));
+                    break;
+                default:
+                    GTA.UI.Notification.Show($"customID ~b~{customID}~s~ ~r~not found~s~" + (playerFound ? $" from {fromPlayer.Username}!" : "!"));
+                    return;
+            }
+        }
+
+        private static void ChatMessage(string from, string message, CancelEventArgs args)
+        {
+            if (!COOPAPI.GetUsername().Equals(from))
+            {
+                return;
+            }
+
+            if (!message.Equals("test"))
+            {
+                return;
+            }
+
+            args.Cancel = true;
+
+            // Send the packet to the server
+            try
+            {
+                byte[] testPacket = new TestPacket() { Content = "Test content" }.Serialize();
+                if (testPacket.Length > 0)
+                {
+                    COOPAPI.SendDataToServer("FirstScript", 0, testPacket);
+                    COOPAPI.LocalChatMessage("FirstScript", "Packet was sent to the server!");
+                }
+                else
+                {
+                    COOPAPI.LocalChatMessage("FirstScript", "Packet[Content] is empty!");
+                }
+            }
+            catch (Exception ex)
+            {
+                GTA.UI.Notification.Show($"~r~{ex.Message}");
+            }
+        }
+    }
+}
